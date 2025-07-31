804da327cfc7217da30bfa16a20af5b79cea9fb8
一秋 <1464074871@qq.com>
2025-08-01 00:52:21 +0800

    fix:函数未定义

--- middleware/rateLimitMiddleware.js
+++ middleware/rateLimitMiddleware.js
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15分钟
-    max: 5, // 限制每个IP 15分钟内最多5次请求
+    windowMs: 10 * 60 * 1000, // 10分钟
+    max: 5, // 限制每个IP 10分钟内最多5次请求
     message: {
         code: 429,
         message: '请求过于频繁，请稍后再试'
